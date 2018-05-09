1f7da2e93a84df9e268bbfe9bbd0e236cc8cc07b
peep02354 <nawapol.lim@hotmail.com>
2018-05-10 00:07:40 +0700

    change String keep place

--- Tweet.java
+++ Tweet.java
@@ -3,13 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Twitter.CSC319oosd;
+package test;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-
 
 /**
  *
@@ -20,6 +19,7 @@ public class Tweet extends TwitterUI {
     public List<String> list = new ArrayList();
     String delim = " \t\n.,:;?!-/()[]\"\'";
     String line;
+    String keep;
     int count = 0;
 
     public Tweet(String n) throws IOException {
@@ -53,16 +53,17 @@ public class Tweet extends TwitterUI {
 
     public void searchWord(String n) {
         int count = 0;
-        String keep = "";
+        keep = "";
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).toLowerCase().contains(n.toLowerCase())) {
                 System.out.println(list.get(i));
-                //keep += list.get(i) + "\n";
+                keep += list.get(i) + "\n";
                 count++;
             }
         }
-        //jTextArea1.setText(keep);
         System.out.println("Found Results : "+count);
+        System.out.println("-----------------------------------------------");
+        
     }
 
     public String getTweet(int i) {
@@ -72,6 +73,8 @@ public class Tweet extends TwitterUI {
     public void getAll() {
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
+
         }
     }
 }
+//matches("(?i)(.*)" + n + "(.*)")
